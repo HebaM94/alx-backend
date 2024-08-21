b4ab43dc575f2f8a0909d363ed5137db9288b4d2
Heba <hebamagde@gmail.com>
2024-08-21 18:12:08 +0300

    Task 1: FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Caching Module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines:
+      - Caching system
+    """
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_item = (list(self.cache_data.keys())).pop(0)
+            print("DISCARD: {}".format(first_item))
+            del self.cache_data[first_item]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

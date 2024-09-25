b35d87e592aef70368d21b159db58ba2da6ad593
Heba <hebamagde@gmail.com>
2024-09-26 01:18:22 +0300

    Task 6: Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,12 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'Test Message' };
+const job = queue.create('push_notification_code', jobData).save((error) => {
+  if (!error) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
